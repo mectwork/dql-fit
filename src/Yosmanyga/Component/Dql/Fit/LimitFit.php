a1904cc7a4ecdcf69bb26357131734b49198eaa5
Manuel Emilio Carpio <mectwork@gmail.com>
2014-07-05 08:50:41 -0500

    Update LimitFit.php

--- src/Yosmanyga/Component/Dql/Fit/LimitFit.php
+++ src/Yosmanyga/Component/Dql/Fit/LimitFit.php
@@ -4,13 +4,24 @@ use Yosmanyga\Component\Dql\Fit\LimitFitInterface;
 
 class LimitFit implements LimitFitInterface
 {
+    /*
+     * @var int
+     */
     private $amount;
 
+    /**
+     * Constructor.
+     *
+     * @param int $amount
+     */
     public function __construct($amount)
     {
         $this->amount = $amount;
     }
 
+    /**
+     * @return int
+     */
     public function getLimit()
     {
         return $this->amount;
